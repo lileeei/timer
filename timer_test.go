003cca249fbae4af82f7d282101f1ab6699f6583
lileeei <lileiwell1993@163.com>
2016-10-20 17:31:01 +0800

    Update timer_test.go

--- timer_test.go
+++ timer_test.go
@@ -11,11 +11,11 @@ var sum int32 = 0
 var N int32 = 100
 var tw *TimerWheel
 
-func now() {
+func callBack() {
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 	atomic.AddInt32(&sum, 1)
 	v := atomic.LoadInt32(&sum)
-	if v == 2*N {
+	if v == N {
 		tw.Stop()
 	}
 
@@ -27,11 +27,10 @@ func TestTimer(t *testing.T) {
 	fmt.Println(timerwheel)
 	var i int32
 	for i = 0; i < N; i++ {
-		timerwheel.AddNode(time.Millisecond*time.Duration(100*i), now)
-		timerwheel.AddNode(time.Millisecond*time.Duration(100*i), now)
+		timerwheel.AddNode(time.Millisecond*time.Duration(1000*i), callBack)
 	}
 	timerwheel.Start()
-	if sum != 2*N {
+	if sum != N {
 		t.Error("failed")
 	}
 }
